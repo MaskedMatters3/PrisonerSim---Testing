f065f683dd67917ab3f265980336d07ab4b44d6e
MaskedMatters3 <120686253+MaskedMatters3@users.noreply.github.com>
2022-12-21 19:57:42 +0000

    Fixing Percentage

--- Percentage.cs
+++ Percentage.cs
@@ -4,6 +4,14 @@ namespace PrisonerSim
     {
         public static void WritePercentage(float percent)
         {   
+            int percentInt;
+
+            if(percent < 1) {
+                percentInt = 0;
+            } else {
+                percentInt = Convert.ToInt32(percent);
+            }
+
             Console.CursorVisible = false; // Set cursor visibility to false
 
             Console.SetCursorPosition(0, 3); // Start cursor at start of line
